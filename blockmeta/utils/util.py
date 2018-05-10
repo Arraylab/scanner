df4c146ce4442d62efb08b071627d5a7b8201cb5
doublespending <doublespending@outlook.com>
2018-05-10 15:19:42 +0800

    fix valid_addr

--- blockmeta/utils/util.py
+++ blockmeta/utils/util.py
@@ -59,5 +59,5 @@ def wrap_error_response(message='', data='', status='failure', code='500'):
 
 def valid_addr(addr):
     addr.strip().lower()
-    return ADDRESS_42_RE.match(addr) or ADDRESS_62_RE.match(addr)
+    return (ADDRESS_42_RE.match(addr) or ADDRESS_62_RE.match(addr)) is not None
 
